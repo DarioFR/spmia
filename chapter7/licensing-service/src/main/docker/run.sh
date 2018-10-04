b4908d0e9e786531c36a71c73bd5a027dff624b9
dario <me@example.com>
2018-10-04 20:22:13 +0200

    chapter 7 bf

--- chapter7/licensing-service/src/main/docker/run.sh
+++ chapter7/licensing-service/src/main/docker/run.sh
@@ -23,7 +23,7 @@ echo "********************************************************"
 echo "Starting License Server with Configuration Service via Eureka :  $EUREKASERVER_URI" ON PORT: $SERVER_PORT;
 echo "License service will use $AUTHSERVER_URI for URI"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
+java -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
      -Dsecurity.oauth2.resource.userInfoUri=$AUTHSERVER_URI               \
